49952afd6c1bd8aafea2530f6ca9caba38083ada
Kyle Buzsaki <kbuzsaki@ucsd.edu>
2016-12-20 14:46:12 -0800

    Implemented cleanDeadLinks

--- cmd/batchprocess/main.go
+++ cmd/batchprocess/main.go
@@ -1,19 +1,22 @@
 package main
 
 import (
-	"errors"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	_ "net/http/pprof"
+	"strings"
 
 	"github.com/kbuzsaki/wikidegree/wiki"
 )
 
 const defaultBatchSize = 1000
 
+var debug bool
+
 func main() {
+	flag.BoolVar(&debug, "debug", false, "print debug output")
 	dbFilename := flag.String("db", wiki.DefaultIndexName, "the boltdb file")
 	batchSize := flag.Int("batch", defaultBatchSize, "number of pages to pass to the processing function at a time")
 	flag.Parse()
@@ -27,7 +30,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	err = process(pr, *batchSize, printShortNames)
+	err = process(pr, *batchSize, cleanDeadLinks)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -41,12 +44,18 @@ func process(pr wiki.PageRepository, batchSize int, f processor) error {
 		return err
 	}
 
+	counter := 0
 	for len(pageBuffer) != 0 {
+		if debug {
+			log.Println("processed", counter)
+		}
+
 		err = f(pr, pageBuffer)
 		if err != nil {
 			return err
 		}
 
+		counter += len(pageBuffer)
 		pageBuffer, err = pr.NextPages(pageBuffer[len(pageBuffer)-1].Title, batchSize)
 	}
 
@@ -64,5 +73,32 @@ func printShortNames(pr wiki.PageRepository, pages []wiki.Page) error {
 }
 
 func cleanDeadLinks(pr wiki.PageRepository, pages []wiki.Page) error {
-	return errors.New("not implemented")
+	for i := range pages {
+		validLinks, err := getValidLinks(pr, pages[i])
+		if err != nil {
+			return err
+		}
+		pages[i].Links = validLinks
+	}
+
+	return pr.SavePages(pages)
+}
+
+func getValidLinks(pr wiki.PageRepository, page wiki.Page) ([]string, error) {
+	var validLinks []string
+
+	for _, link := range page.Links {
+		_, err := pr.LoadPage(link)
+		if err != nil && !strings.HasPrefix(err.Error(), "No entry") {
+			return nil, err
+		} else if err != nil {
+			if debug {
+				log.Printf("found dead link %#v in page %#v\n", link, page.Title)
+			}
+		} else {
+			validLinks = append(validLinks, link)
+		}
+	}
+
+	return validLinks, nil
 }
